Numerical integration fixed.

circlecover/circle.py
@@ -27,9 +27,16 @@ class Circle:
         try:
        	    x = point[0]
             y = point[1]
-       	    return (x- self.Q[0])**2 + (y - self.Q[1])**2 <= self.r**2 + self.r*.0005
+       	    return (x- self.Q[0])**2 + (y - self.Q[1])**2 <=  1.0005*self.r**2  
         except:
             pdb.set_trace()
+
+    def set_radius(self, newradius):
+         self.radius = newradius
+
+    def on(self,point):
+        dist = math.sqrt((point[0] - self.Q[0])**2 + (point[1] - self.Q[1])**2)
+        return np.allclose(dist,self.r,atol=.0001)
             
 
     def area(self):
@@ -174,17 +181,26 @@ class Circle:
 
 
     
-    def compute_slice(self,lines):
+    def compute_polar_slice(self,lines):
         """
-        given a set of lines, compute the area that is inside the circle, using 
-        numerical integration. A slice is a an area enclosed between a circle and a
-        set of lines inside the circ.e
+        Given a set of lines enclosed in this circle which form a slice, compute the area that is enclosed
+        between the lines and the circle using numerical integration. The lines are assumed to form 
+        a polar function i.e. a radial line from the center of the circle can only intersect with a single
+        line in the collection
         
+        Parameters:
+
         lines- a set of connected lines INSIDE the circles. Lines are assumed not to double back
                     on themselves. i.e. no switchback patterns.  Lines are all on one side of the center. 
-        returns:
+                    lines are assumed to be connected and form a POLAR function (i.e. a function of r,theta).
+                    Note that this method will NOT work (throws exception if the lines do not form a polar 
+                    function).
+        Returns:
             - area included in the section between the lines and circumference of the circle 
                 (the slice area). 
+
+        Raises:
+            - Exception if the lines do not form a polar function.
             
         """
         def pol2cart(theta, rho):
@@ -220,11 +236,14 @@ class Circle:
             for l in translated_lines:
                 b,p = l.intersection(ray)
                 if b :
+                    if found :
+                        raise Exception("Contour is not a polar function")
                     d = (p[0]**2 + p[1]**2)
                     if d  < dist:
                         found = True
                         intersection = p
                         dist = d
+
             if not found:
                 # the ray did not intersect with a line.
                 slice_area = circle_area / float(nslices)

circlecover/test/CircleTest.py
@@ -78,7 +78,7 @@ class CircleTest(unittest.TestCase):
         self.assertTrue(b)
         self.assertTrue(len(l) == 2)
         self.assertTrue(c is not None)
-        sliceArea = self.circle.compute_slice([line_segment])
+        sliceArea = self.circle.compute_polar_slice([line_segment])
         self.assertTrue(sliceArea > 0)
         l1 = line.Line(self.circle.get_center(),c.get_p1())
         l2 = line.Line(self.circle.get_center(),c.get_p2())
@@ -95,6 +95,7 @@ class CircleTest(unittest.TestCase):
         self.assertTrue(np.allclose(segmentArea,sliceArea,rtol=1e-3))
         print "segmentArea ", segmentArea, " approximate sliceArea " , sliceArea
         
+        sliceArea = self.circle.compute_polar_slice([line_segment])
 
 
     def testLineIntersectsCircle2(self):
@@ -105,6 +106,8 @@ class CircleTest(unittest.TestCase):
         self.assertTrue(b)
         self.assertTrue(len(l) == 2)
         self.assertTrue(c is not None)
+        self.assertTrue(self.circle.on(c.get_p1()))
+        self.assertTrue(self.circle.on(c.get_p2()))
 
     def testLineMissesCircle(self):
         p1 = [20,80]

circlecover/test/CirclesCoverTest.py
@@ -6,14 +6,41 @@ import circles
 import line
 import pdb
 import random
+import math
+import logging
+import numpy as np
+from line import Line
+
+# set up logging to file - see previous section for more details
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%m-%d %H:%M',
+                    filename='debug.txt',
+                    filemode='w')
+logger = logging.getLogger('circlecover')
+# create file handler which logs even debug messages
+sh = logging.StreamHandler()
+fh = logging.FileHandler('debug.txt')
+fh.setLevel(logging.DEBUG)
+sh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+logger.addHandler(sh)
+
+def areaOfTriangle(l1,l2,l3):
+    a = l1.length()
+    b = l2.length()
+    c = l3.length()
+    s = (a + b + c) /2
+    area = math.sqrt(s*(s -a)*(s-b)*(s-c))
+    return area
 
 def total_area(circle_collection):
     total_area = 0
     for c in circle_collection:
-        total_area = total_area + c.get_radius()**2
+        total_area = total_area + c.area()
     return total_area
 
-def excess_area(circle_collection, covered_lines):
+def slice_area(circle_collection, covered_lines):
     total_area = 0
     for i in range(0,len(circle_collection)):
         total_area = total_area + circle_collection[i].compute_slice(covered_lines[i])
@@ -81,12 +108,14 @@ def printCover(lines,cover,centers,covered_segments,output_file):
         r.append(1)
     f.write("r = 0.5*" + str(r) + ";\n")
     f.write("viscircles (centers,r','Color','g');\n")
-
-    earea = excess_area(cover,covered_segments)
+    logger.debug( "Computing Excess area: ")
+    earea,carea = circles.compute_excess_area(cover,lines)
     f.write("total_area = " + str(total_area(cover)) + ";\n")
-    print "total_area = " , str(total_area(cover))
-    print ("excess_area = "+ str(earea))
-    f.write( "excess_area = " +  str(earea) + ";\n")
+    logger.debug( "total_area = " + str(total_area(cover)))
+    logger.debug ("excess_area = "+ str(earea))
+    logger.debug( "cover_area = " + str(carea))
+    f.write("excess_area = " +  str(earea) + ";\n")
+    f.write("title('GREEDY MAX MIN cover: totalArea = " + str(total_area(cover)) + "; coverArea = " + str(carea) + "; excessArea = " + str(earea) + "');")
     f.close()
     
 
@@ -106,13 +135,6 @@ class CirclesCoverTest(unittest.TestCase):
         l = line.Line([15,45],[38,44])
         c = [ circle.Circle(center=[20,35], radius=13), 
                     circle.Circle(center=[35,40], radius=13) ]
-        #c = [ circle.Circle(center=[20,35], radius=13), 
-        #            circle.Circle(center=[35,40], radius=13), 
-        #            circle.Circle(center=[45,55], radius=8),
-        #            circle.Circle(center=[65,43], radius = 9),
-        #            circle.Circle(center=[53,35], radius=10),
-        #            circle.Circle(center=[40,25], radius=6) ]
-
         b,r = circles.covers_line(c,l)
         self.assertTrue(b)
 
@@ -248,6 +270,35 @@ class CirclesCoverTest(unittest.TestCase):
                     covered = True
             self.assertTrue(covered)
         printCover(line_segments,circ,savedCentrs,segments,"testMinimumCircleSetCoverForLineSetGreedy3.m")
+
+    def testCoverAreaNumericalIntegration(self):
+        """
+        Test the excess area computation.
+        """
+        p1 = [20,30]
+        p2 = [50,60]
+        line_segment = Line(p1,p2)
+        lines = [line_segment]
+        ccle = circle.Circle(center=[40,40], radius=15)
+        b,l,c = ccle.collides(line_segment)
+        self.assertTrue(b)
+        self.assertTrue(len(l) == 2)
+        self.assertTrue(c is not None)
+        l1 = line.Line(ccle.get_center(),c.get_p1())
+        l2 = line.Line(ccle.get_center(),c.get_p2())
+     	angle = math.pi - l1.angle(l2)
+        # The angle that is outside the wedge.
+        remaining_angle =  2*math.pi -   angle
+        # The portion of the circle that is outside the wedge
+        portion_outside = remaining_angle/(2*math.pi)*ccle.area()
+        # The triangle area
+        triangleArea = areaOfTriangle(c,l1,l2)
+        # The following is the exact computation of the excess area.
+        segmentArea = ccle.area() - (triangleArea + portion_outside)
+        # Now compare it with the numerical integration result.
+        segmentArea1,totalarea = circles.compute_excess_area([ccle],lines)
+        self.assertTrue(np.allclose(segmentArea,segmentArea1,rtol=.01))
+        self.assertTrue(np.allclose(ccle.area(),totalarea,rtol=.01))
 
     def testMinimumCircleSetCoverForLineSetGreedyRandom(self):
         line_endpoints = []