59dd24cf098348641650c50dbadb812fa9e6bba9
M. Ranganathan <mranga@nist.gov>
2017-04-24 18:59:51 -0400

    Renamed file.

--- circlecover/compute_antenna_cover.py
+++ circlecover/compute_antenna_cover.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
     with open (protection_region, "r") as myfile:
         data=myfile.readlines()
 
-    # Units are in Km.
+    # Units are in Km. This should be converted to json format.
     esc_loc_x = [x/1000.0 for x in  eval(data[0])]
     esc_loc_y = [x/1000.0 for x in  eval(data[1])]
     ship_loc_x = [x/1000.0 for x in  eval(data[2])]
@@ -56,28 +56,4 @@ if __name__ == "__main__":
         cover = annealr.get_result()
         printcover.printAntennaCover(output_file + "Anneal", interference_contour, possible_centers, cover,coverage_file,60,min_ctr_dist)
 
-    # Return a list of tuples that indicates the coverage.
-    sensor_centers = list(set([c[0] for c in cover]))
-    sensor_loc_x = [r[0] for r in sensor_centers]
-    sensor_loc_y = [r[1] for r in sensor_centers]
-    
-    f = open(output_file + ".txt","w")
-    center_x = [r[0][0] for r in cover]
-    center_y = [r[0][1] for r in cover]
-    indexes  = [r[1] for r in cover]
-    angles  = [r[2] for r in cover]
-    f.write(str(center_x) + "\n")
-    f.write(str(center_y) + "\n")
-    f.write(str(indexes) + "\n")
-    f.write(str(angles) + "\n")
-    f.write(str(sensor_loc_x) + "\n")
-    f.write(str(sensor_loc_y) + "\n")
-    f.write(str(len(sensor_centers)) + "\n")
-    print "Sensor Count = " , str(len(sensor_centers))
-    centers = [c[0] for c in cover]
-    sea_excess_area,land_excess_area = excessarea.compute_excess_area_for_antenna_cover(indexes, angles, centers, coverage_file,
-            possible_centers, interference_contour)
-    f.write(str(sea_excess_area) + "\n")
-    f.write(str(land_excess_area) + "\n")
-    f.close()
 
